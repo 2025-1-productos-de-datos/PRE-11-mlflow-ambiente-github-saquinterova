1224cf6f766100034347cccb1a8b5630efbbd07f
sqv <saquinterova@unal.edu.co>
2025-05-24 22:49:27 +0000

    Try 2

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -13,7 +13,8 @@ def test_01():
     try:
         subprocess.run(
             [
-                "mlflow run https://github.com/jdvelasq/mlflow-wine-quality.git -e knn -P n_neighbors=5"
+                ". ",
+                "run.sh",
             ],
             check=True,
         )
